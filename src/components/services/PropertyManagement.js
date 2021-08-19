33c29eae22f5b41c5585f81f6ec3118d132c228f
Chavez King <chavezking93@gmail.com>
2021-08-19 12:53:42 +0100

    other-service-page-edit

--- src/components/services/PropertyManagement.js
+++ src/components/services/PropertyManagement.js
@@ -30,9 +30,9 @@ class PropertyManagement extends React.Component{
                       </div>
                       </div>
                       <div class="col-sm-8 ml-auto mt-5 col-md-6">
-                          <div class="px-5 ml-5 mt-5 px-sm-0">
+
                             <img style={{height: '500px', width:'500px'}} class="img-fluid rounded" src="https://images.pexels.com/photos/3639504/pexels-photo-3639504.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="..." />
-                          </div>
+
                       </div>
                   </div>
               </div>
@@ -45,9 +45,9 @@ class PropertyManagement extends React.Component{
                   <div class="container-fluid px-5">
                       <div class="row gx-5 px-5 align-items-center justify-content-center">
                       <div class="col-sm-8 mr-auto mt-5 col-md-6">
-                          <div class="px-5 ml-5 mt-5 px-sm-0">
+
                             <img style={{height: '500px', width:'500px'}} class="img-fluid rounded" src="https://images.pexels.com/photos/5847587/pexels-photo-5847587.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="..." />
-                          </div>
+
                       </div>
                           <div class="col-12 col-lg-5">
                           <h2 class=" lh-1 mb-5 fw-bolder">Our property and block management service includes:</h2>
